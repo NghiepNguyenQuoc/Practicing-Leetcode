f12519b71ea86e32f5498e50ec6730cb7e732cc0
Nghiep Nguyen <nghiepnguyenquoc@gmail.com>
2018-09-03 12:12:01 -0500

    Array Structure

---
+++ Leetcode Practice/src/com/nghiepnguyen/Array/Main.java
@@ -0,0 +1,88 @@
+package com.nghiepnguyen.Array;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = new int[]{3, 4, 5, 6, 1, 2};
+        int value=privotedBinarySearch(a, a.length, 6);
+        System.out.print(value);
+    }
+
+    public static void rotateArray(int[] array, int d, int n) {
+        for (int i = 0; i < d; i++) {
+            int temp = array[0];
+            for (int j = 0; j < n - 1; j++) {
+                array[j] = array[j + 1];
+            }
+            array[n - 1] = temp;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + "\t");
+        }
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
+    }
+
+    public static void rotateArray2(int[] array, int d, int n) {
+        for (int i = 0; i < gcd(d, n); i++) {
+            int temp = array[0];
+            int j = i, k;
+            while (true) {
+                k = j + d;
+                if (k >= n)
+                    k = k - n;
+                if (k == i)
+                    break;
+                array[j] = array[k];
+                j = k;
+            }
+            array[j] = temp;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + "\t");
+        }
+    }
+
+    static int privotedBinarySearch(int[] a, int n, int k) {
+        int privot = findPrivot(a, 0, n - 1);
+        if (privot == -1)
+            return binarySearch(a, 0, n - 1, k);
+        if (k == a[privot])
+            return privot;
+        if (a[privot] > k)
+            return binarySearch(a, privot + 1, n - 1, k);
+        return binarySearch(a, 0, privot - 1, k);
+    }
+
+    static int binarySearch(int[] a, int l, int r, int k) {
+        if (l >= r)
+            return -1;
+        int m = (l + r) / 2;
+        if (a[m] == k)
+            return m;
+        if (a[m] < k)
+            return binarySearch(a, l, m - 1, k);
+        return binarySearch(a, m + 1, r, k);
+    }
+
+    static int findPrivot(int[] a, int l, int r) {
+        if (l > r)
+            return -1;
+        if (l == r)
+            return l;
+        int m = (l + r) / 2;
+        if (m < r && a[m] > a[m + 1])
+            return m;
+        if (m > l && a[m] < a[m - 1])
+            return m - 1;
+        if (a[l] >= a[m])
+            return findPrivot(a, l, m - 1);
+        return findPrivot(a, m + 1, r);
+    }
+}
--- Leetcode Practice/src/com/nghiepnguyen/LinkedList/MyLinkedList.java
+++ Leetcode Practice/src/com/nghiepnguyen/LinkedList/MyLinkedList.java
@@ -1,5 +1,9 @@
 package com.nghiepnguyen.LinkedList;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class MyLinkedList {
     Node head;
 
@@ -55,6 +59,152 @@ public class MyLinkedList {
         prev.next = n;
     }
 
+    public Node delete(Node head, int key) {
+        if (head == null) {
+            return null;
+        }
+        if (head.data == key) {
+            head = head.next;
+            return head;
+        }
+        Node cur = head;
+        while (cur.next != null) {
+            if (cur.next.data == key) {
+                cur.next = cur.next.next;
+                break;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    public Node deleteAtPosition(Node head, int position) {
+        if (head == null) {
+            return null;
+        }
+        if (position == 0) {
+            return head.next;
+        }
+
+        Node temp = head;
+        for (int i = 0; temp != null && i < position - 1; i++) {
+            temp = temp.next;
+        }
+
+        if (temp == null || temp.next == null)
+            return null;
+        Node next = temp.next.next;
+        temp.next = next;
+        return head;
+    }
+
+    public int getCount(Node head) {
+        //add code here.
+        int count = 0;
+        if (head == null) {
+            return 0;
+        }
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public int getCount2(Node node) {
+        //add code here.
+        if (head == null) {
+            return 0;
+        }
+        if (node == null)
+            return 0;
+        return getCount2(node.next) + 1;
+    }
+
+    public boolean search(int x) {
+        if (head == null)
+            return false;
+        Node temp = head;
+        for (; temp != null; ) {
+            if (temp.data == x)
+                return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public boolean search2(Node head, int x) {
+        if (head == null)
+            return false;
+
+        if (head.data == x)
+            return true;
+
+        return search2(head.next, x);
+    }
+
+    int getNthFromLast(Node head, int n) {
+        // Your code here
+        if (n <= 0)
+            return -1;
+        int lenght = 0;
+        Node temp = head;
+        for (; temp != null; ) {
+            lenght++;
+            temp = temp.next;
+        }
+        temp = head;
+
+        for (int i = lenght; temp != null; i--) {
+            if (i == n) {
+                return temp.data;
+            }
+            temp = temp.next;
+        }
+        return -1;
+    }
+
+    boolean isPalindrome(Node head) {
+        //Your code here
+        //Your code here
+        List<Integer> array = new ArrayList<>();
+        Node temp = head;
+        while (temp != null) {
+            array.add(temp.data);
+            temp = temp.next;
+        }
+
+        temp = head;
+        int i = array.size() - 1;
+        while (temp != null) {
+            if (temp.data != array.get(i)) {
+                return false;
+            }
+            i--;
+            temp = temp.next;
+        }
+        return true;
+    }
+
+    Node removeDuplicates(Node head) {
+        // Your code here
+        Node temp = head;
+        Node temp2;
+        while (temp != null) {
+            temp2=temp;
+            while(temp2.next!=null){
+                if(temp.data==temp2.next.data){
+                    temp2.next=temp2.next.next;
+                }else{
+                    temp2=temp2.next;
+                }
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+
     public void print() {
         Node n = head;
         while (n != null) {
@@ -68,14 +218,19 @@ public class MyLinkedList {
 
     public static void main(String[] args) {
         MyLinkedList linkedList = new MyLinkedList();
-        linkedList.addHead(new Node(4));
-        linkedList.addHead(new Node(3));
-        linkedList.addHead(new Node(2));
-        linkedList.addHead(new Node(1));
+        linkedList.addTail(new Node(3));
+        linkedList.addTail(new Node(4));
+        linkedList.addTail(new Node(5));
+        linkedList.addTail(new Node(5));
+        linkedList.addTail(new Node(3));
+        linkedList.addTail(new Node(6));
+//        linkedList.addTail(new Node(2));
+//        linkedList.addTail(new Node(2));
 
-        linkedList.addAfter(linkedList.head.next.next, new Node(5));
-
-
+//        linkedList.deleteAtPosition(0);
+        linkedList.removeDuplicates(linkedList.head);
         linkedList.print();
+//        System.out.print("\n\n" + linkedList.search2(linkedList.head, 4));
+
     }
 }
