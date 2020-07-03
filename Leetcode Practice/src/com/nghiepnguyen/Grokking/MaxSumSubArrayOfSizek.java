202b639fffbf2e6e86e02737d4b0dbe7bd29b24c
Nguyen Quoc Nghiep <quocnghiep.nguyen21@t-mobile.com>
2020-07-03 16:51:17 -0700

    replace findSum to findSum2

--- Leetcode Practice/src/com/nghiepnguyen/Grokking/MaxSumSubArrayOfSizek.java
+++ Leetcode Practice/src/com/nghiepnguyen/Grokking/MaxSumSubArrayOfSizek.java
@@ -3,22 +3,26 @@ package com.nghiepnguyen.Grokking;
 public class MaxSumSubArrayOfSizek {
     public static void main(String[] args) {
         int[] arr = new int[]{2, 1, 5, 1, 3, 2};
-        System.out.println("max sum sub: " + findSum(arr, 3));
+        System.out.println("max sum sub: " + findSum2(arr, 3));
     }
 
-    private static int findSum(int[] arr, int k) {
-        int sum = 0;
-        for (int i = 0; i <= arr.length - k; i++) {
-            int subSum = 0;
-            for (int j = i; j < i + k; j++) {
-                subSum += arr[j];
-            }
-            System.out.println("sub sum: " + subSum);
-            if (subSum > sum) {
-                sum = subSum;
+    /**
+     * better approach using slide window
+     * */
+    private static int findSum2(int[] arr, int k) {
+        int maxSum = 0;
+        int windowSum = 0;
+        int windowStart = 0;
+        for (int i = 0; i < arr.length; i++) {
+            windowSum += arr[i];
+            System.out.println("sub sum: " + windowSum);
+            if (i >= k - 1) {
+                maxSum = Math.max(maxSum, windowSum);
+                windowSum -= arr[windowStart];
+                windowStart++;
             }
         }
-        return sum;
+        return maxSum;
     }
 
 }
